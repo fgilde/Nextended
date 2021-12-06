7d394d02e9447e3d6535c373f32fd279a7a1bd0b
Florian Gilde <fgilde@gmail.com>
2021-12-06 11:10:44 +0100

    Switch to nerdbank versioning

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCompany("Florian Gilde")]
-[assembly: AssemblyProduct("NiceExtensions for net core")]
-[assembly: AssemblyCopyright("Copyright (c) Florian Gilde")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("6.0.0")]
-[assembly: AssemblyFileVersion("6.0.0")]
+//[assembly: AssemblyCompany("Florian Gilde")]
+//[assembly: AssemblyProduct("NiceExtensions for net core")]
+//[assembly: AssemblyCopyright("Copyright (c) Florian Gilde")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+//[assembly: AssemblyVersion("6.0.0")]
+//[assembly: AssemblyFileVersion("6.0.0")]
