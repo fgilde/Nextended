71908982e566a978ad8b3f3af7542f90eb88e384
Florian Gilde <fgilde@gmail.com>
2022-05-02 11:42:24 +0200

    Add small simple action scope

---
+++ Nextended.Core/Scopes/ActionScope.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nextended.Core.Scopes;
+
+public class ActionScope: IDisposable
+{
+    private readonly Action actionOut;
+
+    public ActionScope(Action actionIn, Action actionOut)
+    {
+        this.actionOut = actionOut;
+        actionIn();
+    }
+
+    public void Dispose()
+    {
+        actionOut();
+    }
+}
