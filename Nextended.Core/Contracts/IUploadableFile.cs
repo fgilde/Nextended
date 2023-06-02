597a470d5ba89f516d6dc3073f2891ba654ab4ce
Florian Gilde <fgilde@gmail.com>
2023-06-02 11:16:12 +0200

    Interface for upload file

---
+++ Nextended.Core/Contracts/IUploadableFile.cs
@@ -0,0 +1,10 @@
+﻿namespace Nextended.Core.Contracts;
+
+public interface IUploadableFile
+{
+    public string FileName { get; set; }
+    public string Extension { get; set; }
+    public string ContentType { get; set; }
+    public byte[] Data { get; set; }
+    public string Url { get; set; }
+}
