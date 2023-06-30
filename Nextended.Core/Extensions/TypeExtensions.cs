e77c9c1be61c6166646dd40781c9a33c3ad205d2
Florian Gilde <fgilde@gmail.com>
2023-06-30 11:58:52 +0200

    Type ext for tuple and value tuple

--- Nextended.Core/Extensions/TypeExtensions.cs
+++ Nextended.Core/Extensions/TypeExtensions.cs
@@ -52,16 +52,14 @@ namespace Nextended.Core.Extensions
         public static bool IsCollection(this Type type)
             => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ICollection<>) || type.GetInterfaces().Any(IsCollection);
 
+
         public static bool IsNullableEnum(this Type t)
             => Nullable.GetUnderlyingType(t) is { IsEnum: true };
 
         
         public static bool IsExpression(this Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Expression<>);
 
-        public static bool IsNullableType(this Type type)
-        {
-            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
-        }
+        public static bool IsNullableType(this Type type) => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
 
         public static bool IsNullable(this Type type)
         {
@@ -69,161 +67,153 @@ namespace Nextended.Core.Extensions
             return !type.IsValueType || IsNullableType(type);
         }
 
-        public static bool IsNullableOf<T>(this Type type)
+        public static bool IsNullableOf<T>(this Type type) 
+            => IsNullableType(type) && type.GetGenericArguments().FirstOrDefault() == typeof(T);
+
+        public static bool IsString(this Type input) 
+            => input == typeof(string);
+
+        public static bool IsDecimal(this Type input) 
+            => input == typeof(decimal);
+
+        public static bool IsInt(this Type input) 
+            => input == typeof(int);
+
+        public static bool IsDateTime(this Type input) 
+            => input == typeof(DateTime);
+
+        public static bool IsBool(this Type input) 
+            => input == typeof(bool);
+
+        public static bool IsNullableDecimal(this Type input) 
+            => input == typeof(decimal?);
+
+        public static bool IsNullableInt(this Type input) 
+            => input == typeof(int?);
+
+        public static bool IsNullableDateTime(this Type input) 
+            => input == typeof(DateTime?);
+
+        public static bool IsNullableBool(this Type input) 
+            => input == typeof(bool?);
+
+        public static bool IsType(this Type input) 
+            => input == typeof(Type);
+
+        public static bool IsIEnumerable(this Type input) 
+            => typeof(IEnumerable).IsAssignableFrom(input);
+
+        public static bool IsEnumerableOrArray(this Type type) 
+            => typeof(IEnumerable).IsAssignableFrom(type) && type != typeof(string) || type.IsArray;
+
+        public static bool IsEnumerable(this Type type)
+            => type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>) || type.GetInterfaces().Any(IsCollection);
+
+
+        public static bool IsIList(this Type input) 
+            => typeof(IList).IsAssignableFrom(input);
+
+        public static bool IsNotString(this Type input) 
+            => input != typeof(string);
+
+        public static bool IsNotDecimal(this Type input) 
+            => input != typeof(decimal);
+
+        public static bool IsNotInt(this Type input) 
+            => input != typeof(int);
+
+        public static bool IsNotDateTime(this Type input) 
+            => input != typeof(DateTime);
+
+        public static bool IsNotBool(this Type input) 
+            => input != typeof(bool);
+
+        public static bool IsNotNullableDecimal(this Type input) 
+            => input != typeof(decimal?);
+
+        public static bool IsNotNullableInt(this Type input) 
+            => input != typeof(int?);
+
+        public static bool IsNotNullableDateTime(this Type input) 
+            => input != typeof(DateTime?);
+
+        public static bool IsNotNullableBool(this Type input) 
+            => input != typeof(bool?);
+
+        public static bool IsNotType(this Type input) 
+            => input != typeof(Type);
+
+        public static bool IsNotIEnumerable(this Type input) 
+            => !typeof(IEnumerable).IsAssignableFrom(input);
+
+        public static bool IsNotIList(this Type input) 
+            => !typeof(IList).IsAssignableFrom(input);
+
+        public static bool IsStruct(this Type t) 
+            => t.IsValueType && !t.IsPrimitive && !t.IsEnum && t != typeof(ValueType) && (!t.IsGenericType || t.GetGenericTypeDefinition() != typeof(Nullable<>));
+
+        public static bool IsReadOnlyStruct(this Type t) 
+            => IsStruct(t) && t.GetProperties().All(prop => !prop.CanWrite);
+
+        public static object CreateInstance(this Type input) 
+            => ReflectionHelper.CreateInstance(input);
+
+        public static T CreateInstance<T>(this Type input) 
+            => ReflectionHelper.CreateInstance<T>();
+
+        public static T CreateInstance<T>(this Type input, params object[] args) 
+            => (T)Activator.CreateInstance(input, args);
+
+        public static bool IsSubclassOfInterfaceOf<TInterface>(this Type toCheck) 
+            => IsSubclassOfInterfaceOf(toCheck, typeof(TInterface));
+
+        public static bool IsKeyValuePair(this Type t)
         {
-            return IsNullableType(type) && type.GetGenericArguments().FirstOrDefault() == typeof(T);
+            if (t.IsGenericType)
+            {
+                var def = t.GetGenericTypeDefinition();
+                return def == typeof(KeyValuePair<,>);
+            }
+            return false;
+        }
+        
+        public static bool IsTupleOrValueTuple(this Type t)
+            => IsTuple(t) || IsValueTuple(t);
+        
+        public static bool IsTuple(this Type t)
+        {
+            if (t.IsGenericType)
+            {
+                var def = t.GetGenericTypeDefinition();
+                return def == typeof(Tuple<>) ||
+                       def == typeof(Tuple<,>) ||
+                       def == typeof(Tuple<,,>) ||
+                       def == typeof(Tuple<,,,>) ||
+                       def == typeof(Tuple<,,,,>) ||
+                       def == typeof(Tuple<,,,,,>) ||
+                       def == typeof(Tuple<,,,,,,>) ||
+                       def == typeof(Tuple<,,,,,,,>);
+            }
+            return false;
         }
 
-        public static bool IsString(this Type input)
+        public static bool IsValueTuple(this Type t)
         {
-            return input == typeof(string);
+            if (t.IsGenericType)
+            {
+                var def = t.GetGenericTypeDefinition();
+                return def == typeof(ValueTuple<>) ||
+                       def == typeof(ValueTuple<,>) ||
+                       def == typeof(ValueTuple<,,>) ||
+                       def == typeof(ValueTuple<,,,>) ||
+                       def == typeof(ValueTuple<,,,,>) ||
+                       def == typeof(ValueTuple<,,,,,>) ||
+                       def == typeof(ValueTuple<,,,,,,>) ||
+                       def == typeof(ValueTuple<,,,,,,,>);
+            }
+            return false;
         }
 
-        public static bool IsDecimal(this Type input)
-        {
-            return input == typeof(decimal);
-        }
-
-        public static bool IsInt(this Type input)
-        {
-            return input == typeof(int);
-        }
-
-        public static bool IsDateTime(this Type input)
-        {
-            return input == typeof(DateTime);
-        }
-
-        public static bool IsBool(this Type input)
-        {
-            return input == typeof(bool);
-        }
-
-        public static bool IsNullableDecimal(this Type input)
-        {
-            return input == typeof(decimal?);
-        }
-
-        public static bool IsNullableInt(this Type input)
-        {
-            return input == typeof(int?);
-        }
-
-        public static bool IsNullableDateTime(this Type input)
-        {
-            return input == typeof(DateTime?);
-        }
-
-        public static bool IsNullableBool(this Type input)
-        {
-            return input == typeof(bool?);
-        }
-
-        public static bool IsType(this Type input)
-        {
-            return input == typeof(Type);
-        }
-
-        public static bool IsIEnumerable(this Type input)
-        {
-            return typeof(IEnumerable).IsAssignableFrom(input);
-        }
-
-        public static bool IsEnumerableOrArray(this Type type)
-        {
-            return typeof(IEnumerable).IsAssignableFrom(type) && type != typeof(string) || type.IsArray;
-        }
-
-        public static bool IsIList(this Type input)
-        {
-            return typeof(IList).IsAssignableFrom(input);
-        }
-
-        public static bool IsNotString(this Type input)
-        {
-            return input != typeof(string);
-        }
-
-        public static bool IsNotDecimal(this Type input)
-        {
-            return input != typeof(decimal);
-        }
-
-        public static bool IsNotInt(this Type input)
-        {
-            return input != typeof(int);
-        }
-
-        public static bool IsNotDateTime(this Type input)
-        {
-            return input != typeof(DateTime);
-        }
-
-        public static bool IsNotBool(this Type input)
-        {
-            return input != typeof(bool);
-        }
-
-        public static bool IsNotNullableDecimal(this Type input)
-        {
-            return input != typeof(decimal?);
-        }
-
-        public static bool IsNotNullableInt(this Type input)
-        {
-            return input != typeof(int?);
-        }
-
-        public static bool IsNotNullableDateTime(this Type input)
-        {
-            return input != typeof(DateTime?);
-        }
-
-        public static bool IsNotNullableBool(this Type input)
-        {
-            return input != typeof(bool?);
-        }
-
-        public static bool IsNotType(this Type input)
-        {
-            return input != typeof(Type);
-        }
-
-        public static bool IsNotIEnumerable(this Type input)
-        {
-            return !typeof(IEnumerable).IsAssignableFrom(input);
-        }
-
-        public static bool IsNotIList(this Type input)
-        {
-            return !typeof(IList).IsAssignableFrom(input);
-        }
-
-        public static bool IsStruct(this Type t)
-        {
-            return t.IsValueType && !t.IsPrimitive && !t.IsEnum && t != typeof(ValueType) && (!t.IsGenericType || t.GetGenericTypeDefinition() != typeof(Nullable<>));
-        }
-        public static bool IsReadOnlyStruct(this Type t)
-        {
-            return IsStruct(t) && t.GetProperties().All(prop => !prop.CanWrite);
-        }
-
-        public static object CreateInstance(this Type input)
-        {
-            return ReflectionHelper.CreateInstance(input);
-        }
-
-        public static T CreateInstance<T>(this Type input)
-        {
-            return ReflectionHelper.CreateInstance<T>();
-        }
-
-        public static T CreateInstance<T>(this Type input, params object[] args)
-        {
-            return (T)Activator.CreateInstance(input, args);
-        }
-
-        public static bool IsSubclassOfInterfaceOf<TInterface>(this Type toCheck) => IsSubclassOfInterfaceOf(toCheck, typeof(TInterface));
         public static bool IsSubclassOfInterfaceOf(this Type toCheck, Type interfaceType)
         {
             var interfaces = toCheck.GetInterfaces();
