9d76ae63f2b6f785c8f475ecb4c341968dacc73c
Florian Gilde <fgilde@gmail.com>
2023-10-09 18:29:51 +0200

    Add path to uploadableFile

--- Nextended.Core/Contracts/IUploadableFile.cs
+++ Nextended.Core/Contracts/IUploadableFile.cs
@@ -7,4 +7,5 @@ public interface IUploadableFile
     public string ContentType { get; set; }
     public byte[] Data { get; set; }
     public string Url { get; set; }
+    public string Path { get; set; }
 }
