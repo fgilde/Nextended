c74d3378b8da2c393bd0a22e5067685f3073cd22
Florian Gilde <fgilde@gmail.com>
2023-10-30 14:41:17 +0100

    Add size to IUploadableFile

--- Nextended.Core/Contracts/IUploadableFile.cs
+++ Nextended.Core/Contracts/IUploadableFile.cs
@@ -11,6 +11,7 @@ public interface IUploadableFile
     public byte[] Data { get; set; }
     public string Url { get; set; }
     public string Path { get; set; }
+    public long Size { get; set; }
 
     /// <summary>
     /// If file is not loaded, loads it from Url
