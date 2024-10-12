81603a82e58ae7f5dd7ae7c04d4ddb5a172f168d
Florian Gilde <fgilde@gmail.com>
2024-10-12 18:11:34 +0200

    work an async child loading

--- Nextended.Core/Types/Hierarchical.cs
+++ Nextended.Core/Types/Hierarchical.cs
@@ -1,24 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
 using Nextended.Core.Extensions;
 
 namespace Nextended.Core.Types;
 
-public abstract class Hierarchical<T> : IHierarchical<T>
-    where T : Hierarchical<T>
+public abstract class Hierarchical<T> : IHierarchical<T>, IChildInfo
+    where T : Hierarchical<T>, new()
 {
     private HashSet<T> _children;
 
-    public T Parent { get; set; }
+    public Func<T, CancellationToken, Task<HashSet<T>>> LoadChildrenFunc { get; set; } = null;
 
+    private CancellationTokenSource? _loadCancel = null;
+
+    public Action<IHierarchical<T>, HashSet<T>> OnChildrenLoaded { get; set; }
+
+    public T Parent { get; set; }
+    public bool IsLoading { get; set; }
     public IEnumerable<T> Path => (this as T).Path();
 
     public bool IsExpanded { get; set; }
 
     public virtual HashSet<T> Children
     {
-        get => _children;
+        get
+        {
+            if (LoadChildrenFunc != null)
+            {
+                if (_loadCancel == null)
+                {
+                    _loadCancel = new CancellationTokenSource();
+                    //IsLoading = true;
+                    LoadChildrenFunc(this as T, _loadCancel.Token).ContinueWith(t =>
+                    {
+                        LoadChildrenFunc = null;
+                        Children = t.Result;
+                        //IsLoading = false;
+                        OnChildrenLoaded?.Invoke(this, t.Result);
+                    });
+                }
+
+                return [new T {IsLoading = true}];
+            }
+            return _children;
+        }
         set
         {
             _children = value;
@@ -35,7 +63,7 @@ public abstract class Hierarchical<T> : IHierarchical<T>
             hierarchical.Parent = (T)this;
     }
 
-    public virtual bool HasChildren => Children?.Any() == true;
+    public virtual bool HasChildren => LoadChildrenFunc != null || Children?.Any() == true;
 
     public virtual bool ContainsChild(T entry)
     {
@@ -43,24 +71,38 @@ public abstract class Hierarchical<T> : IHierarchical<T>
     }
 }
 
+
 public interface IHierarchical<T> where T : IHierarchical<T>
 {
+    Func<T, CancellationToken, Task<HashSet<T>>> LoadChildrenFunc { get; }
+    public Action<IHierarchical<T>, HashSet<T>> OnChildrenLoaded { get; set; }
     public HashSet<T> Children { get; }
+    public bool IsLoading { get; }
     public T Parent { get; }
+}
+
+public interface IChildInfo
+{
+    public bool HasChildren { get; }
 }
 
 public static class HierarchicalExtensions
 {
+    public static bool ValidForRecursion<T>(this T node) where T : IHierarchical<T>
+    {
+        return !node.IsLoading && node.LoadChildrenFunc == null && node.HasChildren();
+    }
+
     public static IEnumerable<T> Find<T>(this IEnumerable<T> entries, Func<T, bool> predicate)
         where T : IHierarchical<T>
     {
-        return entries.Recursive(h => h.Children.EmptyIfNull()).Where(predicate);
+        return entries.Recursive(h => h.Children.EmptyIfNull(), a => a.ValidForRecursion()).Where(predicate);
     }
 
     public static IEnumerable<T> Find<T>(this T entry, Func<T, bool> predicate)
         where T : IHierarchical<T>
     {
-        return entry.Children.EmptyIfNull().Recursive(h => h.Children.EmptyIfNull()).Where(predicate);
+        return entry.Children.EmptyIfNull().Recursive(h => h.Children.EmptyIfNull(), a => a.ValidForRecursion()).Where(predicate);
     }
 
     public static bool IsInPathOf<T>(this T node, T entry)
@@ -71,7 +113,7 @@ public static class HierarchicalExtensions
 
     public static bool HasChildren<T>(this T node)
             where T : IHierarchical<T> =>
-            node?.Children?.Any() == true;
+        node is IChildInfo info ? info.HasChildren : node?.Children?.Any() == true;
 
     public static IEnumerable<T> Siblings<T>(this T node)
         where T : IHierarchical<T>
@@ -88,7 +130,7 @@ public static class HierarchicalExtensions
 
     public static bool Contains<T>(this T node, T entry)
         where T : IHierarchical<T> =>
-        node?.Children?.EmptyIfNull().Recursive(n => n?.Children?.EmptyIfNull()).Contains(entry) == true;
+        node?.Children?.EmptyIfNull().Recursive(n => n?.Children?.EmptyIfNull(), a => a.ValidForRecursion()).Contains(entry) == true;
 
     private static IEnumerable<T> GetPath<T>(T item)
         where T : IHierarchical<T>
