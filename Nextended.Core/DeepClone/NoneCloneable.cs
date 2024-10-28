bb0535d94fcc80fe51ae73987c1e0c926a8d7487
Florian Gilde <fgilde@gmail.com>
2024-10-28 16:54:18 +0100

    Bring Back deep clone ns

---
+++ Nextended.Core/DeepClone/NoneCloneable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nextended.Core.DeepClone
+{
+    /// <summary>
+    /// Apply this to properties that cant be cloned, eg ImageSource and other controls.
+    /// Those property will still be copied insted of cloning
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class NoneCloneable : Attribute
+    {
+    }
+}
