d71227de48c597183441248638607d87e30e198e
Florian Gilde <fgilde@gmail.com>
2025-04-21 14:57:50 +0200

    add something

---
+++ Nextended.Core/IGeneratedLocalization.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Nextended.Core;
+
+public interface IGeneratedLocalization
+{
+    IDictionary<string, string> GetAll(CultureInfo cultureInfo = null);
+    IDictionary<string, string> GetAll(bool includeParents, CultureInfo cultureInfo = null);
+    string GetString(string key, CultureInfo cultureInfo = null);
+    string GetString(string key, bool includeParents, CultureInfo cultureInfo = null);
+}
